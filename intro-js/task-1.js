84ec24b699d893eb9f7555c27a7272cc6f47fd18
mdrony5134 <rony830388@gmail.com>
2024-08-31 22:53:51 +0600

    intro-js task compelete

---
+++ intro-js/task-1.js
@@ -0,0 +1,6 @@
+let apple = 350;
+let orange = 350;
+let money = 1000;
+let total = apple + orange;
+
+console.log(money - total);
---
+++ intro-js/task-2.js
@@ -0,0 +1,8 @@
+let Mathematics = 75.25;
+let Biology = 65;
+let Chemistry = 80;
+let Physics = 35.45;
+let Bangla = 99.5;
+
+let average = (Mathematics + Biology + Chemistry + Physics + Bangla) / 5;
+console.log(average.toFixed(2));
