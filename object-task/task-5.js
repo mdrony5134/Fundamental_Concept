6fd04d8860ec79ba32769be7ee4df26977a5cfcc
mdrony5134 <rony830388@gmail.com>
2024-09-08 18:04:16 +0600

    object task finished

--- object-task/task-5.js
+++ object-task/task-5.js
@@ -13,21 +13,18 @@
 
 // Output:
 
-
 // key: name | type:  string
 // key: age | type:  number
 // key: city | type:  string
 // key: isStudent | type:  boolean
 
-
 let myObject = {
+  name: "John Doe",
+  age: 25,
+  city: "Example City",
+  isStudent: true,
+};
 
-    name: 'John Doe',
-    age: 25,
-    city: 'Example City',
-    isStudent: true
-    };
-
-    for (const key in myObject) {
-        console.log("key:",key + " / type:" , typeof(myObject[key]))
-    }
+for (const key in myObject) {
+  console.log("key:", key + " / type:", typeof myObject[key]);
+}
